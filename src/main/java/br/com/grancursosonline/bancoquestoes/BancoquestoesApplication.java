db5c74e7049d978a7c0feb8c3dd2c32ba6873bbd
Bruno Carneiro <bruno.carneiro312@gmail.com>
2020-07-23 12:28:53 -0300

    setup

---
+++ src/main/java/br/com/grancursosonline/bancoquestoes/BancoquestoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.grancursosonline.bancoquestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoquestoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoquestoesApplication.class, args);
+    }
+
+}
